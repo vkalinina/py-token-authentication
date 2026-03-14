4b5b042b46588f4dacdfe3ff588cfe2655b7656e
Viktoria Kalinina <viktoriakalinina@orderberry.de>
2026-03-14 11:04:54 +0100

    Fix: permissions

--- cinema/permissions.py
+++ cinema/permissions.py
@@ -5,7 +5,7 @@ class IsAdminOrIfAuthenticatedReadOnly(BasePermission):
 
     def has_permission(self, request, view):
         return bool(
-            request.method in SAFE_METHODS
+            request.method in SAFE_METHODS and request.user.is_authenticated
         ) or (
             request.user and request.user.is_staff
         )
--- cinema/views.py
+++ cinema/views.py
@@ -2,7 +2,6 @@ from datetime import datetime
 
 from django.db.models import F, Count
 from rest_framework import viewsets, mixins
-from rest_framework.authentication import TokenAuthentication
 from rest_framework.pagination import PageNumberPagination
 from rest_framework.permissions import IsAuthenticated
 
